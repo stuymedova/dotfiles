d2f30c1d34feebbfa6515ba296954bd82f45f2f5
Sofya Tuymedova <stuymedova@gmail.com>
2023-12-08 00:14:07 +0300

    Update vscode settings and extensions

--- vscode/install-vscode-extensions.sh
+++ vscode/install-vscode-extensions.sh
@@ -7,9 +7,15 @@
 pkglist=(
 	dbaeumer.vscode-eslint
 	denoland.vscode-deno
+	eamodio.gitlens
 	esbenp.prettier-vscode
 	GitHub.github-vscode-theme
-	mhutchie.git-graph
+	GitHub.remotehub
+	github.vscode-github-actions
+	GitHub.vscode-pull-request-github
+	ms-vscode.azure-repos
+	ms-vscode.remote-repositories
+	Orta.vscode-jest
 	svelte.svelte-vscode
 	VisualStudioExptTeam.intellicode-api-usage-examples
 	VisualStudioExptTeam.vscodeintellicode
